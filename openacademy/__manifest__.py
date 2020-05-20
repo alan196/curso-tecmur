7c1419effa07697c0d0509d1159ed3002e25783a
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-05-20 16:11:57 -0500

    [ADD] openacademy: create module with basic files

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020, Jarsa
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Openacademy',
+    'summary': 'Module to teach how to make a module',
+    'version': '12.0.1.0.0',
+    'category': 'Tecmur',
+    'website': 'https://www.jarsa.com.mx/',
+    'author': 'Jarsa, Odoo Community Association (OCA), Tecmur',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [],
+    'demo': [],
+}
