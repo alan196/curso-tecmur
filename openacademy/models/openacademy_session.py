35503b8311ad58284081b2d9478f2c3314932d28
Luis Muro <luis.muroo@gmail.com>
2020-06-03 19:17:48 -0500

    [ADD]Adding Decorators, progressbar and python constraint

--- openacademy/models/openacademy_session.py
+++ openacademy/models/openacademy_session.py
@@ -1,7 +1,7 @@
 # Copyright 2020, Jarsa
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import api, exceptions, fields, models
 
 
 class OpenacademySession(models.Model):
@@ -9,10 +9,49 @@ class OpenacademySession(models.Model):
     _description = "OpenAcademy Sessions"
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(defaults=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner')
+    active = fields.Boolean(default=True)
+    instructor_id = fields.Many2one(
+        'res.partner', domain=[
+            '|',
+            ('instructor', '=', True),
+            ('category_id.name', 'ilike', "Teacher")]
+    )
     course_id = fields.Many2one(
         'openacademy.course', ondelete='cascade', required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    taken_seats = fields.Float(compute='_compute_taken_seats')
+
+    @api.depends('seats', 'attendee_ids')
+    def _compute_taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
+    @api.onchange('seats', 'attendee_ids')
+    def _onchange_verify_valid_seats(self):
+        if self.seats < 0:
+            self.seats = 0
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendees",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _constrain_check_instructor_not_in_attendees(self):
+        for r in self:
+            # Por uqé hay un and de la misma variable??
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError("A session's instructor can't be an attendee")
