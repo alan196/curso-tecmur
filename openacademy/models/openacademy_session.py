4e96db5de1ee9039d7a5c7944b58dd3d1be7948c
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-06-04 14:18:19 -0500

    [ADD] openacademy: add calendar view to openacademy.sesion

--- openacademy/models/openacademy_session.py
+++ openacademy/models/openacademy_session.py
@@ -1,6 +1,8 @@
 # Copyright 2020, Jarsa
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
+from datetime import timedelta
+
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 
@@ -24,6 +26,8 @@ class OpenacademySession(models.Model):
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(compute='_compute_taken_seats')
     active = fields.Boolean(default=True)
+    end_date = fields.Date(
+        compute='_compute_end_date', inverse='_inverse_end_date', store=True)
 
     @api.depends('seats', 'attendee_ids')
     def _compute_taken_seats(self):
@@ -58,3 +62,25 @@ class OpenacademySession(models.Model):
             if rec.instructor_id and rec.instructor_id in rec.attendee_ids:
                 raise ValidationError(
                     "A session's instructor can't be an attendee")
+
+    @api.depends('start_date', 'duration')
+    def _compute_end_date(self):
+        for rec in self:
+            if not (rec.start_date and rec.duration):
+                rec.end_date = rec.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            duration = timedelta(days=rec.duration, seconds=-1)
+            rec.end_date = rec.start_date + duration
+
+    def _inverse_end_date(self):
+        for rec in self:
+            if not (rec.start_date and rec.end_date):
+                continue
+
+            # Compute the difference between dates, but:
+            # Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            rec.duration = (rec.end_date - rec.start_date).days + 1
